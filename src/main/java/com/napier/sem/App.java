2d26f20f3f7765da2c39846c84fb9f5cf0414963
LMAO445 <kwy9914566sht@gmail.com>
2025-09-23 13:43:01 +0630

    t3

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,37 @@
 package com.napier.sem;
 
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Connect to MongoDB on localhost, port 27000
+        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27000")) {
+            // Get a database - will be created if it doesn't exist
+            MongoDatabase database = mongoClient.getDatabase("mydb");
+
+            // Get a collection from the database
+            MongoCollection<Document> collection = database.getCollection("test");
+
+            // Create a document to store
+            Document doc = new Document("name", "Kevin Sim")
+                    .append("class", "Software Engineering Methods")
+                    .append("year", "2021")
+                    .append("result", new Document("CW", 95).append("EX", 85));
+
+            // Add document to collection
+            collection.insertOne(doc);
+
+            // Check document in collection
+            Document myDoc = collection.find().first();
+            if (myDoc != null) {
+                System.out.println(myDoc.toJson());
+            } else {
+                System.out.println("No document found!");
+            }
+        }
     }
 }
