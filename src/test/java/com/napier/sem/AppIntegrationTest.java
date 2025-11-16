7fe39e98d83392963b438c7fca4ae9b5d33d3c48
LMAO445 <kwy9914566sht@gmail.com>
2025-11-16 16:07:49 +0630

    fix lab 8 2

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -22,9 +22,9 @@ public class AppIntegrationTest {
     @Test
     void testGetEmployee()
     {
-        Employee emp = app.getEmployee(255530);
-        assertEquals(255530, emp.emp_no);
-        assertEquals("Ronghao", emp.first_name);
-        assertEquals("Garigliano", emp.last_name);
+        Employee emp_no = app.getEmployee(255530);
+        assertEquals(255530, emp_no.emp_no);
+        assertEquals("Ronghao", emp_no.first_name);
+        assertEquals("Garigliano", emp_no.last_name);
     }
 }
